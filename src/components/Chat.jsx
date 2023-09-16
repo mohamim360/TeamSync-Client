33aa6e86829c8db8c9f4ff2286dda8a2e24b0eab
mohamim360 <mohamim360@gmail.com>
2023-09-16 09:54:36 +0600

    adding async and loading

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,21 +1,34 @@
+
 import React, { useState } from "react";
 
 function Chat() {
   const [message, setMessage] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function fetchMessageHandler() {
-    fetch("http://localhost:3000/chat/messages")
-      .then((res) => res.json())
-      .then((data) => {
-        setMessage(data.messages[0]);
-      });
+  async function fetchMessageHandler() {
+    setIsLoading(true);
+    const response = await fetch("http://localhost:3000/chat/messages");
+    const data = await response.json();
+    setMessage(data.messages[0]);
+    setIsLoading(false);
   }
   return (
     <>
       <div>
-        <button className="btn-success p-3" onClick={fetchMessageHandler}>Send</button>
-				<h5>{message.user}</h5>
-        <p>{message.message}</p>
+        <button className="btn-success p-3" onClick={fetchMessageHandler}>
+          Send
+        </button>
+        <div>
+          {!isLoading && (
+            <div>
+              <h5>{message.user}</h5>
+              <p>{message.message}</p>
+            </div>
+          )}
+          {isLoading && (
+            <span className="loading loading-bars loading-lg"></span>
+          )}
+        </div>
       </div>
     </>
   );
