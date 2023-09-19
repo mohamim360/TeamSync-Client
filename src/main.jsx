633289f990aebba1b1740176b4e85978684f57c4
mohamim360 <mohamim360@gmail.com>
2023-09-19 16:16:31 +0600

    PrivateRoute on Chat

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Chat/>,
+        element: <PrivateRoute><Chat/></PrivateRoute>,
       },
       {
         path: "/login",
